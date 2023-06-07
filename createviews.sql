faf95f30b4d2150901183941f9912cf6fb8fe6c3
Howard Ginsburg <howardsginsburg@gmail.com>
2023-06-07 13:33:55 -0400

    updates to view creation to reflect new object structure.

--- createviews.sql
+++ createviews.sql
@@ -1,5 +1,7 @@
 CREATE OR ALTER VIEW EventData
 AS SELECT [id], [subject], [resourceuri], [status]
+CREATE OR ALTER VIEW EventData
+AS SELECT [id], [subject], [resourceuri], [action], Convert(datetime2, createdDate, 126) as [createddate], Convert(datetime2, completedDate, 126) as [completeddate]
 FROM OPENROWSET(​PROVIDER = 'CosmosDB',
                 CONNECTION = 'Account=$(cosmosAccountName);Database=TenantMonitoring',
                 OBJECT = 'EventLog',
@@ -8,23 +10,28 @@ FROM OPENROWSET(​PROVIDER = 'CosmosDB',
 WITH (  id    varchar(100),
         subject     varchar(1000) '$.eventgriddata.subject',
         resourceUri varchar(1000) '$.eventgriddata.data.resourceUri',
-        status      varchar(15)
+        action      varchar(1000) '$.eventgriddata.data.authorization.action',
+        createdDate varchar(8000) '$.createdDate',
+        completedDate varchar(8000) '$.completedDate'
 ) AS rows
 GO
 
-CREATE OR ALTER VIEW EventLogs
-AS SELECT [id], [Date] as logdate, [log] as logdetail
+CREATE OR ALTER VIEW JobLogs
+AS SELECT [id], [name], [description],  [api], [status], Convert(datetime2, rundate, 126) as [rundate]
 FROM OPENROWSET(​PROVIDER = 'CosmosDB',
                 CONNECTION = 'Account=$(cosmosAccountName);Database=TenantMonitoring',
                 OBJECT = 'EventLog',
                 SERVER_CREDENTIAL = 'tenantmonitoring'
 ) 
 WITH (  id    varchar(100),
-        logs varchar(max) '$.logs'
+        jobs varchar(max) '$.jobsConfig.jobs'
 ) AS rows
-CROSS APPLY OPENJSON ( logs )
+CROSS APPLY OPENJSON ( jobs )
 WITH (
-    Date datetime2,
-    log varchar(max)
-) AS logrows
+    Name varchar(1000) '$.Name',
+    Description varchar(max) '$.Description',
+    api varchar(max) '$.Api',
+    status varchar(100) '$.Status',
+    rundate varchar(8000) '$.RunDate'
+) AS jobrows
 GO
